df5a085b01e6e99fdf4330848debd06f716d1ef3
Venla Katainen <71126341+venlakatainen@users.noreply.github.com>
2022-12-09 09:23:59 +0200

    Add files via upload

---
+++ src/explore_hello_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { explore_hello_backend } from "../../declarations/explore_hello_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await explore_hello_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
